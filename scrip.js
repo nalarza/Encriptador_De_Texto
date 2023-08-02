f8afb93f186c6d609940d9bf611b10777dd6a681
Nelson Andres Alarza Andrade <nalarzaandrade@gmail.com>
2023-08-02 09:41:35 -0500

    se optimisa la vista de la pantalla

---
+++ scrip.js
@@ -0,0 +1,39 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar(){
+    const textEncriptado = encriptar(textArea.value)
+    mensaje.value = textEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptado;
+}
+
+function btnDesencriptar(){
+    const textEncriptado = desencriptar(textArea.value)
+    mensaje.value = textEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+function desencriptar(stringDesencriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptado;
+}
+
